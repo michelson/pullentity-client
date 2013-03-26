ae8c4f89a1c5579b2bb84d7c91ff6ef3419e2112
miguel michelson <miguelmichelson@gmail.com>
2013-03-26 01:45:28 -0300

    domain for production

--- lib/pullentity-client/generate/auth.rb
+++ lib/pullentity-client/generate/auth.rb
@@ -10,7 +10,8 @@ module Pullentity
         no_tasks {
 
           def domain
-            "http://pullentity.dev:3000"
+            "http://pullentity.com"
+            #"http://pullentity.dev:3000"
           end
 
           def login(email)
