d1442fc81dc24cda8f800cf7773fb5e2699271fb
miguel michelson <miguelmichelson@gmail.com>
2013-03-25 21:59:09 -0300

    cli auth option writing yaml

--- lib/pullentity-client/cli.rb
+++ lib/pullentity-client/cli.rb
@@ -35,6 +35,33 @@ module Pullentity
         system "bundle exec middleman server"
       end
 
+      map %w(r) => 'server'
+      desc "login ", "retreives token"
+      long_desc "pullentity-client login user pass"
+      def login(email)
+        begin
+          hsh = YAML.load_file(location + "pullentity.yml")
+        rescue => e
+          say "Error: #{e}, make shure you are inside pulleneity project"
+          say "and pullentity.yml file is created"
+        end
+        say("In order to retreive your authentication token we need your pullentity password")
+        password = ask("password: ")
+        uri = URI.parse("http://pullentity.dev:3000/api/v1/login.json")
+        http = Net::HTTP.new(uri.host, uri.port)
+        request = Net::HTTP::Post.new(uri.request_uri)
+        request.set_form_data({"email" => email, "password" => password  })
+        response = http.request(request)
+        if response.status == 200
+          say "Your token is:"
+          say response.body
+          hsh["auth_token"] = response.body
+          File.open("dailyfocus/pullentity.yml", "w"){|f| YAML.dump(hsh, f)}
+        else
+          say response.body
+        end
+      end
+
       map %w(b) => 'build'
       desc "build ", "builds middleman/ios/android app"
       long_desc "Build pullentity-client static files (middleman), See 'pullentity-client help build' for more information.
